18ebff03764780ec0cdf9011fb77723239cc58a6
Yanu <59870900+khusanova@users.noreply.github.com>
2026-04-28 22:17:24 +0200

    Add setup.py script to create database

---
+++ setup.py
@@ -0,0 +1,8 @@
+import os
+from app import app, db_path
+from data_models import db
+
+if not os.path.exists(db_path):
+    os.makedirs(os.path.dirname(db_path), exist_ok=True)
+    with app.app_context():
+        db.create_all()
