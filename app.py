ac6e4da8ae8c06a914841fc03ec32da151314131
Yanu <59870900+khusanova@users.noreply.github.com>
2026-04-28 22:16:56 +0200

    Add db import to app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from data_models import db
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, 'data', 'library.sqlite')
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+db.init_app(app)
 
 
 @app.route('/')
