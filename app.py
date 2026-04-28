1ac5296cb1c38847bbe6f3d94aa1709b94097595
Yanu <59870900+khusanova@users.noreply.github.com>
2026-04-28 21:28:58 +0200

    fixup! Add database URI to app.config

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, 'data', 'library.sqlite')
-app.config['SQLALCHEMY_DATABASE_URI'] = (f"sqlite:///{db_path}"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
 
 
 @app.route('/')
-)def welcome():  # put application's code here
+def welcome():  # put application's code here
     return 'Welcome to my library!'
 
 
