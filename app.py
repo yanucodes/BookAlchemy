afd4cb3ff4b583023c67c60cb4c3a7b8bc52363a
Yanu <59870900+khusanova@users.noreply.github.com>
2026-04-28 23:29:47 +0200

    Add route add_author

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 import os
-from flask import Flask
-from data_models import db
+from datetime import date
+from flask import Flask, render_template, request
+from data_models import db, Author
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, 'data', 'library.sqlite')
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
 db.init_app(app)
+
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    if request.method == 'POST':
+        name = request.form['name']
+        birth_date = date.fromisoformat(request.form['birthdate'])
+        date_of_death_input = request.form['date_of_death']
+        date_of_death = (date.fromisoformat(date_of_death_input) if
+            date_of_death_input else None)
+        new_author = Author(name=name, birth_date=birth_date,
+                            date_of_death=date_of_death)
+        db.session.add(new_author)
+        db.session.commit()
+
+    return render_template('add_author.html')
 
 
 @app.route('/')
